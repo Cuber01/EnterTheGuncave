4e022da5cf8f52c0e776f29845010f0b44ea3642
Cuber01 <johnmallard631@gmail.com>
2021-12-06 14:28:48 +0100

    init

---
+++ EnterTheGuncave/AssetLoader.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace EnterTheGuncave
+{
+    public class AssetLoader
+    {
+        Dictionary<string, Texture2D> textures = new Dictionary<string, Texture2D>();
+        private readonly ContentManager contentManager;
+
+        public AssetLoader(ContentManager contentManager)
+        {
+            this.contentManager = contentManager;
+        }
+        
+        public void loadTextures()
+        {
+            textures.Add("player", contentManager.Load<Texture2D>("assets/player"));
+            textures.Add("enemy", contentManager.Load<Texture2D>("assets/enemy"));
+            textures.Add("arrow", contentManager.Load<Texture2D>("assets/arrow"));
+            textures.Add("stone", contentManager.Load<Texture2D>("assets/stone"));
+        }
+    }
+}
