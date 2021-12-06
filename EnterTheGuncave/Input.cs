664c8893ccc8b930b759b34a79880b10ee5e76a8
Cuber01 <johnmallard631@gmail.com>
2021-12-06 18:36:40 +0100

    and mouse handling

--- EnterTheGuncave/Input.cs
+++ EnterTheGuncave/Input.cs
@@ -5,10 +5,16 @@ namespace EnterTheGuncave
     public class Input
     {
         public static KeyboardState keyboardState;
+        public static MouseState mouseState;
 
         public static void updateKeyboardState()
         {
             keyboardState = Keyboard.GetState();
+        }
+
+        public static void updateMouseState()
+        {
+            mouseState = Mouse.GetState();
         }
     }
 }
