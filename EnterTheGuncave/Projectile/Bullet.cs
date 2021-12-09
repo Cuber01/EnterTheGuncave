fb3a400fe6965edc0a80e07fd43a6c4f9365f4a8
Cuber01 <johnmallard631@gmail.com>
2021-12-09 21:48:09 +0100

    Finished the bullet, no implementation yet

--- EnterTheGuncave/Projectile/Bullet.cs
+++ EnterTheGuncave/Projectile/Bullet.cs
@@ -11,6 +11,7 @@ namespace EnterTheGuncave.Projectile
         private int myWidth;
         private int myHeight;
 
+        private Vector2 target;
         private int speed;
         private int penetration;
         private int lifetime;
@@ -19,22 +20,25 @@ namespace EnterTheGuncave.Projectile
         private Vector2 position;
         private Vector2 velocity;
 
-        public Bullet(int speed = 1, int penetration = 1, int lifetime = 500, int damage = 5)
+        public Bullet(Vector2 target, int speed = 1, int penetration = 1, int lifetime = 500, int damage = 5)
         {
             this.texture = AssetLoader.textures["bullet"];
             this.myWidth  = texture.Width  / EnterTheGuncave.scale;
             this.myHeight = texture.Height / EnterTheGuncave.scale;
-            
+
+            this.target = target;
             this.speed = speed;
             this.penetration = penetration;
             this.lifetime = lifetime;
             this.damage = damage;
+            
+            goToPoint(target);
         }
 
         
         public void update()
         {
-            
+            move();
         }
 
         public void draw()
